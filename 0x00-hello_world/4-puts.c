b4efee6a8b14da3aa843d691ef5ca650152a5866
NdundaM <dmumondunda@gmail.com>
2022-12-17 06:39:14 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- *  main - Prints a given phrase
- *  Return: 0 if executed properly, non-zero otherwise
+ *  main - Entry
+ *  Return: always 0
  */
-
-int  main(void)
-{      
-	puts("\"programming is like building a multilingual puzzle");
+int main(void)
+{      	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
